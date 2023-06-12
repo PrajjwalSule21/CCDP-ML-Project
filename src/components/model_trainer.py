51ee7937d8b89dbe7eeccfd19840db81d25c9e65
PrajjalSule21 <suleprajjwal21@gmail.com>
2023-06-12 17:26:07 +0530

    trigger prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -94,7 +94,7 @@ class ModelTrainer:
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models, parameter=None)
+                                             models=models, parameter=params)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
